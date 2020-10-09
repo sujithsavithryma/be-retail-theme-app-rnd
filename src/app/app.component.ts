03e57bbd9d4af6c1c82a8b2d0fd91834040e6784
Sujith M A <sujith.ma@beretail.in>
2020-10-09 12:50:48 +0530

    FLEXREF-214: components page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BeNav } from 'dist/be-theme/lib/core/layout/toolbar-nav/toolbar-nav.model';
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'be-theme-app';
+
+	navs: BeNav[] = [
+		{ name: 'Components', link: 'components' }
+	];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,25 @@ import { AppComponent } from './app.component';
 // import { BeThemeModule } from 'projects/be-theme/src/public-api';
 import { BeLayoutModule } from 'projects/be-theme/src/lib/core/layout/publi-api';
 import { BeComponentsModule } from 'projects/be-theme/src/lib/core/components/public-api';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ComponentsComponent } from './components/components.component';
+
+const routes: Routes = [
+	{ path: 'components', component: ComponentsComponent },
+	{ path: '', redirectTo: 'components', pathMatch: 'full' }
+];
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		ComponentsComponent
 	],
 	imports: [
 		BrowserModule,
 		// BeThemeModule,
 		BeLayoutModule,
 		BeComponentsModule,
-		RouterModule.forRoot([])
+		RouterModule.forRoot(routes)
 	],
 	providers: [],
 	bootstrap: [AppComponent]
